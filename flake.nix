1a02a833f2d5e1ca8104340559fd778edbd232c9
Michael Webb <michaelwebb76@gmail.com>
2026-06-10 15:59:16 +1000

    Update nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Tech Team Haskell Trial - 2025 Edition";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.11-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-26.05-darwin";
     flake-parts.url = "github:hercules-ci/flake-parts";
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
